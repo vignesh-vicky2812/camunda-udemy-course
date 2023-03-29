eb10bc08d54731c27fabafef5000cb01f5ebc879
vignesh-vicky2812 <116265109+vignesh-vicky2812@users.noreply.github.com>
2023-03-29 19:52:16 +0530

    h

---
+++ aws-kms/src/main/java/com/vzc/awskms/AwsKmsApplication.java
@@ -0,0 +1,13 @@
+package com.vzc.awskms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsKmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsKmsApplication.class, args);
+	}
+
+}
---
+++ aws-kms/src/test/java/com/vzc/awskms/AwsKmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vzc.awskms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsKmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
